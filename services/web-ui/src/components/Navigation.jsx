61bcec19b2d74ca9a986730cbc16e5fc29c72c05
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2019-11-19 20:37:29 +0100

    Created navbar

--- services/web-ui/src/components/Navigation.jsx
+++ services/web-ui/src/components/Navigation.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import {NavLink} from "react-router-dom";
+import {BrowserRouter as Router, NavLink} from "react-router-dom";
 
 export const Navigation = ({auth}) => (
-    <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-            <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30"
-                 className="d-inline-block align-top" alt="" />
-                Bootstrap
-        </a>
+    <nav className="navbar navbar-dark bg-dark">
+        <div className={"row"}>
+            <div className={"col"}>
+                <a className="navbar-brand" href="#">
+                    <img src="https://static.delijn.be/Images/dl-logo_tcm3-1040.svg" width="30" height="30"
+                         className="d-inline-block align-top logo" alt=""/>
+                    De Lijn Rater
+                </a>
+            </div>
+        </div>
+        <div className={"row"}>
+            <div className={"col float-left"}>
+                <Router>
+                    <NavLink className={"nav-normal"} to={"/test"}>Stop ratings</NavLink>
+                    <NavLink className={"nav-normal"} to={"/test"}>Vehicle ratings</NavLink>
+                    {/*<NavLink className={""} to={"/test"}>Profile</NavLink>*/}
+                </Router>
+            </div>
+            <div className={"col-1 float-right"}>
+                <Router>
+                    <NavLink className="nav-login nav-item" to={"/login"}>Login</NavLink>
+                </Router>
+            </div>
+        </div>
     </nav>
 );
